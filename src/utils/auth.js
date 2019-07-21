2909e6cf218c36421c6b53d4f4870365b6ea8f78
wxyzcctv <2846629226@qq.com>
2019-07-21 15:41:52 +0800

    修改一个权限设置时存在的一个bug

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,12 +4,13 @@ export function getCurrentAuthority() {
 // 这里是定一个获取权限的函数，本来是该向后端获取的，这里就做了一个假的权限
 export function check(authority) {
   const current = getCurrentAuthority();
-  return current.some(item => authority.include(item));
-  // some表示数组current中是否有满足括号中函数的元素，如果current是中有元素包含了authority，那么就返回这个元素
+  return current.some(item => authority.includes(item));
+  // some表示数组current中是否有满足括号中函数的元素，如果current是中有元素包含了authority，那么就返回true或者false
 }
+// 以上是做校验的，传入的是一个权限，这个权限就是需要校验的
 
 export function isLoging() {
   const current = getCurrentAuthority();
   return current && current[0] !== "guest";
 }
-// 设置登录权限，当current存在且不等于guest的时候就允许登录
+// 设置登录权限，当有前权限，当前权限中不是guest的时候就返回一个true，就通过，能正常登录
