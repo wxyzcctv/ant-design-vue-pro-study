cd6e1a8ac05058accb38f93040411779917e0c25
wxyzcctv <2846629226@qq.com>
2019-07-28 20:37:33 +0800

    实现通过父组件传递数据到子组件中

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="chartDom" style="height:400px;"></div>
+  <div ref="chartDom"></div>
 </template>
 
 <script>
@@ -7,39 +7,34 @@ import echarts from "echarts";
 import debounce from "lodash/debounce";
 import { addListener, removeListener } from "resize-detector";
 export default {
+  props: {
+    option: {
+      stype: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    // option(val) {
+    //   this.chart.setOption(val);
+    // }
+    // 这里通过监听函数watch来监听option的值的变化,一旦option发生了变化就会将,就从新渲染传入的option的值,
+    // 但是这个时候监听的是option整体的值,而不是其中某个数组的值,如果要进行监听option内层数组的值的变化,需要进行的是深度监听
+    // 以下是深度监听的写法,但是深度监听有一个很大的弊端就是非常的耗浏览器的性能,而且对于chart来说数据量可能会非常的大
+    option: {
+      handler(val) {
+        this.chart.setOption(val);
+      },
+      deep: true
+    }
+  },
   // 通过debouch函数进行防抖操作，这里的resize是经过create函数中防抖处理后的resize
   created() {
     this.resize = debounce(this.resize, 300);
   },
   mounted() {
-    // 基于准备好的dom，初始化echarts实例
-    this.chart = echarts.init(this.$refs.chartDom);
     // 一旦监听到了dom元素的大小发生了变化，就执行resize函数
     addListener(this.$refs.chartDom, this.resize);
-
-    // 指定图表的配置项和数据
-    var option = {
-      title: {
-        text: "ECharts 入门示例"
-      },
-      tooltip: {},
-      legend: {
-        data: ["销量"]
-      },
-      xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "销量",
-          type: "bar",
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
-    };
-    // 使用刚指定的配置项和数据显示图表。
-    this.chart.setOption(option);
+    this.renderChart();
   },
   beforeDestroy() {
     // 生命周期销毁的时候需要将Dom节点从新规范大小，于此同时还需要将定义的chart进行销毁，防止内存泄漏
@@ -52,8 +47,14 @@ export default {
       // 这个时候通过打日志的形式可以看出这里在销毁和创建的时候会多次执行这个函数，这个时候就需要使用防抖
       console.log("resize");
       this.chart.resize();
-    }
+    },
     // 这里定义了resize函数是将图表进行从新的规范大小
+    renderChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(this.$refs.chartDom);
+      this.chart.setOption(this.option);
+      //   这里是通过父组件将this.option的内容传递进来
+    }
   }
 };
 </script>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,14 +1,54 @@
 <template>
   <div>
-    <Chart />
+    <Chart :option="this.chartOption" style="height:400px" />
   </div>
 </template>
 
 <script>
+import random from "lodash/random";
 import Chart from "../../components/Chart";
+import { setInterval, clearInterval } from "timers";
 export default {
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
   components: {
     Chart
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => random(100)
+        // 这里的意思是将数组中的所有的数进行从新的排列,从新排列后的数是在100以内的
+      );
+      this.chartOption = { ...this.chartOption };
+      // 这一行代码就是将改变过后的值赋给新的量,这个量就可以在子组件中不通过深度监听实现而实现变量的更新
+    }, 3000);
+  },
+  // 每次使用过后就消除这个定时器,不然很耗性能
+  beforeDestroy() {
+    clearInterval(this.interval);
   }
 };
 </script>
