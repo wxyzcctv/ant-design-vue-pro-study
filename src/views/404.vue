e3d880c3ef78eca5c20f1830ad303e3178e38489
wxyzcctv <2846629226@qq.com>
2019-08-11 17:30:59 +0800

    通过函数式组件的形式加入图标，这里需要改cli关于svg的配置

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,11 +1,27 @@
 <template>
   <div style="text-align: center;">
     <IconFont type="icon-icon-404" style="font-size: 100px;" />
+    <br />
+    <!-- <img :src="logo" /> -->
+    <Logo />
   </div>
 </template>
 
 <script>
-export default {};
+// import logo from "@/assets/海豚.svg";
+import Logo from "@/assets/logo.svg";
+export default {
+  components: {
+    Logo
+  }
+
+  // data() {
+  //   return {
+  //     logo
+  //   };
+  // }
+  // 这是一个通过img的方式引入的一个方式
+};
 </script>
 
 <style></style>
